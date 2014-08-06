13065123b00dc9b18346e9ac4c011296a80e85df
kjnsn <thekjnsn@gmail.com>
2014-08-06 15:56:28 +1000

    GET /admin/candidates works according to spec

--- index.js
+++ index.js
@@ -2,16 +2,43 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var redis = require("redis"),
-        client = redis.createClient();
+        r = redis.createClient();
+var underscore = require("underscore");
 
 
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/public'));
+
+
 // Admin
 
 // Candidates
-app.get('/admin/candidates', function(req, res){
-  res.status(200).json({error: "not implemented yet"});
+app.get('/admin/candidates', function(req, res) {
+  // Candidate ids stored in set "candidate"
+  r.smembers("candidates", function(err, candidates) {
+
+    // Hash info stored in key "candidate:id"
+    var candidates_json = underscore.reduce(
+      candidates, // list of candidates
+      function(memo, uid) {
+        r.hgetall('candidate:' + uid, function(err, reply) {
+          var x = reply;
+          x.id = uid;
+          r.smembers('election:' + x.id, function(err, reply) {
+            x.elections = reply;
+            memo.push(x);
+            if (memo.length == candidates.length) {
+              res.json(memo);
+              res.end();
+            }
+            return memo;
+          });
+        });
+      },
+    []);
+
+  });
 });
 
 app.post('/admin/candidates', function(req, res){
@@ -47,7 +74,26 @@ app.post('/admin/votingcodes', function(req, res){
 // Public
 
 app.post('/votingcode', function(req, res){
-  res.send('not implemented yet');
+  // Try and find the code as a hash
+  if (! req.body.hasOwnProperty("code")) {
+    res.status(400).json({error: "code required"});
+    return;
+  }
+
+  var code;
+  r.hgetall(req.body.code, function(err, reply) {
+    code = reply;
+  });
+
+  if (code == null || code.used) {
+    res.status(404).json({error:"invalid code"})
+  }
+
+  // Set the code as being used
+  code.used = true;
+  r.hmset(req.body.code, code);
+
+  res.json({token: "iwishiwasreal"});
 });
 
 app.get('/elections', function(req, res){
